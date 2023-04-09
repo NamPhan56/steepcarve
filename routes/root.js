072f1441d7b2c20143e3d72df0727883e4374ed0
Nam Phan <namphan56@gmail.com>
2023-04-08 23:45:22 -0400

    Added logger and error hander middleware, added error page and routing, added cookie parser module

---
+++ routes/root.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+//Get requests
+//only match if the requested route is only a slash
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+    //look in the views folder for the "index.html" file
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+
 const PORT = process.env.POR || 3500
 
+app.use(logger)
+//allow json receving and parsing
+app.use(express.json())
+
+app.use(cookieParser())
+
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+
+app.all('*',(req,res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, '/views', '404.html'))
+    }
+    else if (req.accepts('json')){
+        res.json({message: '404 Not Found'})
+    }
+    else {
+        res.type('txt').send(('404 not found'))
+    }
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
