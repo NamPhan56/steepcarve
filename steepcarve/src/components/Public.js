9a2c9c0027f788ba040b8887ef3466dfc43744c1
Nam Phan <namphan56@gmail.com>
2023-04-16 19:43:25 -0400

    setting up routes in React

---
+++ steepcarve/src/components/Public.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+        const content = (
+            <section className="public">
+                <header><h1>Welcome to Steep Carve!</h1></header>
+
+                <main>
+                </main>
+            </section>
+        )
+        return content
+}
+
+export default Public
